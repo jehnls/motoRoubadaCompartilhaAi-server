095c053aa9b7ace8497819206528425c8fff740a
jehnls <jeferson_nls@hotmail.com>
2020-08-28 12:41:14 -0300

    If applied this commit will remove the methods cretead and edit, because don't necessary in API-REST

--- app/Controllers/Http/MotorcycleController.js
+++ app/Controllers/Http/MotorcycleController.js
@@ -17,20 +17,10 @@ class MotorcycleController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
+  async index({ request, response, view }) {
   }
 
-  /**
-   * Render a form to be used for creating a new motorcycle.
-   * GET motorcycles/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
-  }
+
 
   /**
    * Create/save a new motorcycle.
@@ -40,7 +30,7 @@ class MotorcycleController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store({ request, response }) {
   }
 
   /**
@@ -52,20 +42,9 @@ class MotorcycleController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
+  async show({ params, request, response, view }) {
   }
 
-  /**
-   * Render a form to update an existing motorcycle.
-   * GET motorcycles/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
 
   /**
    * Update motorcycle details.
@@ -75,7 +54,7 @@ class MotorcycleController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update({ params, request, response }) {
   }
 
   /**
@@ -86,7 +65,7 @@ class MotorcycleController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy({ params, request, response }) {
   }
 }
 
