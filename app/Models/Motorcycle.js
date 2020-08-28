8a060a203d0cad8e95df82dd59adc871b4a69ec9
jehnls <jeferson_nls@hotmail.com>
2020-08-28 12:23:32 -0300

    If applied this commit will relation motorcycle with user and images with motorcycle

--- app/Models/Motorcycle.js
+++ app/Models/Motorcycle.js
@@ -4,6 +4,17 @@
 const Model = use('Model')
 
 class Motorcycle extends Model {
+
+    //Creted relation "1-1", a motorcyle to an user
+    user() {
+        return this.belongsTo('App/Models/User')
+    }
+
+    //Creted relation n-1, many images to a motorcycle
+    images() {
+        return this.hasMany('App/Models/Image')
+    }
+
 }
 
 module.exports = Motorcycle
