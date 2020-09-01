1705d72a2a6983e3f2bfe10246b068a588adf962
jehnls <jeferson_nls@hotmail.com>
2020-09-01 15:07:03 -0300

    If applied this commit will calculate of navegation, transform the amount in kilometer

--- app/Models/Motorcycle.js
+++ app/Models/Motorcycle.js
@@ -2,8 +2,25 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
+const Database = use('Database')
 
 class Motorcycle extends Model {
+
+    //Creted for locate motorcyles next the User
+    static scopeNearBY(query, latitude, longitude, distance) {
+
+        //Sintax The Haversine for navegation, trasform the amount in kilometer
+        const haversine = `(6371 * acos(cos(radians(${latitude}))
+        * cos(radians(latitude))
+        * cos(radians(longitude)
+        - radians(${longitude}))
+        + sin(radians(${latitude}))
+        * sin(radians(latitude))))`
+
+        return query
+            .select('*', Database.raw(`${haversine} as distance`))
+            .whereRaw(`${haversine} < ${distance}`)
+    }
 
     //Creted relation "1-1", a motorcyle to an user
     user() {
